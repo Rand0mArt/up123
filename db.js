819aa5f2a553fa21d931d5e379249729a7aae736
Dhash Dhasher <dhashdhasher@192.168.1.173>
2026-02-08 21:08:37 -0600

    fix: update supabase url

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 
-const SUPABASE_URL = 'https://pwvudnaueupwbhfprmyg.supabase.co';
+const SUPABASE_URL = 'https://czdialdmmsfiguuxmojr.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3dnVkbmF1ZXVwd2JoZnBybXlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1ODMyNTMsImV4cCI6MjA4NjE1OTI1M30.KcER7BEPPoClh5LKpNxO6axrtAoXNDnERJOZy1jpsjE';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
